440a84b2da8297869caae131114bc72af5aa680f
Thales Euflauzino <thaleseuflauzino@gmail.com>
2024-08-28 21:25:18 -0300

    feat: add home2

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I have a great passion for technology and I am specializing in
+              backend development, with a focus on Machine Learning and
+              Artificial Intelligence.
+              <br />
+              <br />I have knowledge in
+              <i>
+                <b className="purple">
+                  {" "}
+                  Python, C/C++, PHP, HTML, CSS and JS.{" "}
+                </b>{" "}
+                and also in frameworks like{" "}
+                <b className="purple">Django and React.js</b>
+                <br />
+                <br />I have knowledge also in &nbsp;
+                <i>
+                  <b className="purple">
+                    Api Rest, Data Structures, Databases, Object-Oriented
+                    Programming,
+                  </b>{" "}
+                  and
+                  <b className="purple"> Testing (Unit and Integration).</b>
+                </i>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  Machine Learning and Artificial Intelligence.
+                </b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/thaleseuflauzino"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/thaleseuflauzino/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/thales.santos8"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
