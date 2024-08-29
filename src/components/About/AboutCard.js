d75ce1a51b2d7db5d977bbbeeeecb2f4fc878551
Thales Euflauzino <thaleseuflauzino@gmail.com>
2024-08-28 21:24:53 -0300

    feat: add aboutme card

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Thales Euflauzino </span>
+            from <span className="purple"> Brasília, Brasil.</span>
+            <br />
+            I'm currently student of Software Engineering at Universidade de Brasília (UNB).
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Read Books
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
