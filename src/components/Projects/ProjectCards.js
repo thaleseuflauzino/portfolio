32850398921db5059cea533cde7954e9415b7554
Thales Euflauzino <120137721+thaleseuflauzino@users.noreply.github.com>
2025-06-18 19:48:48 -0300

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -34,7 +34,7 @@ function ProjectCards(props) {
             style={{ marginLeft: "10px" }}
           >
             <CgWebsite /> &nbsp;
-            {"Demo"}
+            {"Website"}
           </Button>
         )}
       </Card.Body>
